e5e632fc6a95c0b3f2d0a3cbd566a92c450cf5f9
Pedro Melo <pmelo2005@me.com>
2024-09-22 21:32:46 -0700

    adding lab 1 to branch 3

---
+++ Lab Assigment 01 by Pedro Melo/Lab Assigment 01 by Pedro Melo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace First_Code_in_Game_Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Binary is a base 2 representation");
+            Console.WriteLine("0, 1, 10, 100, 1000, 10000, 100000, 1000000, 1100100, 11111111");
+            Console.WriteLine("Decimal is a base 10 representation");
+            Console.WriteLine("0, 1, 2, 4, 8, 16, 32, 64, 100, 255");
+            Console.WriteLine("Hexadecimal is a base 16 representation and those system are necessary to count to 15 to represent numbers 10 to 15 and letters A to F");
+            Console.WriteLine("0, 1, 2, 4, 8, 10, 20, 40, 64, FF");
+            Console.WriteLine("Goodbye");
+            Console.WriteLine("Pedro Melo 09/06/2024");
+        }
+    }
+}
