a9bfd01b16d973b3b4dd0a5160f589d6f8b12c5a
Pedro Melo <pmelo2005@me.com>
2024-10-04 05:25:18 -0700

    Added the GD12_1133_A1_PedroMelo
    Added the GD12_1133_A1_PedroMelo

---
+++ GD12_1133_A1_PedroMelo/GD12_1133_A1_PedroMelo/Scripts/GameManager.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GD12_1133_A1_PedroMelo.Scripts {
+    internal class GameManager {
+        string userStringWantsToPlay = "";
+        bool userBoolWantsToPlay;
+
+        // Creating the Array
+        int[] rounds = new int[1];
+
+        // Creating the Instances
+        Player player1 = new Player(); // Player1 Instance
+        Player player2 = new Player();// Player2 Instance
+
+        // Function Play that does call others functions
+        internal void Play() {
+            Intro();
+            UserWantsToPlay();
+            GetPlayerName();
+            Description();
+            UserUnderstoodRules();
+            GameLoop();
+            End();
+        }
+
+        // Function GameLoop run the game logic
+        internal void GameLoop() {
+            player1.playerTotalScore = 0;
+            player2.playerTotalScore = 0;
+            // Assign value to the Array
+            rounds[0] = 1;
+
+            // For loop that execute the rounds
+            for (rounds[0] = 1; rounds[0] <= 4; rounds[0]++) {
+                Console.WriteLine(); // blank space
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("ROUND: " + rounds[0]);
+                Console.ResetColor();
+                // Call function Turns
+                Turns();
+            }
+            // Call function WinLose
+            WinLose();
+        }
+
+        // Function Turns that run the turns logic
+        void Turns() {
+            // Call function PlayerPickDice of player1
+            player1.PlayerPickDice();
+            // Call function PlayerPickDice of player2
+            player2.PlayerPickDice();
+
+            Console.WriteLine(); // blank space
+            Console.Write("On ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("(Round " + rounds[0] + ") ");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(player1.playerName);
+            Console.ResetColor();
+            Console.WriteLine(" scored: " + player1.playerScore);
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(player2.playerName);
+            Console.ResetColor();
+            Console.WriteLine(" scored: " + player2.playerScore);
+            // Call function RoundResult
+            RoundResult();
+        }
+
+        // Function RoundResult calculates which player won the round
+        void RoundResult() {
+            if (player1.playerScore > player2.playerScore) { // Display Player 1 won the round
+                Console.WriteLine(); // blank space
+                Console.Write("On ");
+                Console.Write("Round " + rounds[0] + " ");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write(player1.playerName);
+                Console.ResetColor();
+                Console.WriteLine(" won this round !!");
+                player1.playerTotalScore++;
+            }
+            else if (player1.playerScore < player2.playerScore) { // Display Player 2 won the round
+                Console.WriteLine(); // blank space
+                Console.Write("On ");
+                Console.Write("Round " + rounds[0] + " ");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write(player2.playerName);
+                Console.ResetColor();
+                Console.WriteLine(" won this round !!");
+                player2.playerTotalScore++;
+            }
+            else if (player1.playerScore == player2.playerScore) { // Anyone won
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Anyone won this round");
+                Console.ResetColor();
+                Console.WriteLine(); // blank space
+            }
+            player1.playerScore = 0;
+            player2.playerScore = 0;
+        }
+
+        // Function WinLose calculates which player won the game
+        void WinLose() {
+            player1.playerTotalScore--;
+            player2.playerTotalScore--;
+            Console.WriteLine(); // blank space
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(player1.playerName);
+            Console.ResetColor();
+            Console.WriteLine(" your total score is: " + player1.playerTotalScore);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(player2.playerName);
+            Console.ResetColor();
+            Console.WriteLine(" your total score is: " + player2.playerTotalScore);
+
+            if (player1.playerTotalScore > player2.playerTotalScore) {
+                Console.WriteLine(); // blank space
+                Console.Write("GOOD JOB ");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write(player1.playerName);
+                Console.ResetColor();
+                Console.WriteLine(" YOU WON THE GAME !!!");
+            }
+            else if (player1.playerTotalScore < player2.playerTotalScore) {
+                Console.WriteLine(); // blank space
+                Console.Write("GOOD JOB ");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write(player2.playerName);
+                Console.ResetColor();
+                Console.WriteLine(" YOU WON THE GAME !!!");
+            }
+            else if (player1.playerTotalScore == player2.playerTotalScore) {
+                Console.WriteLine(); // blank space
+                Console.WriteLine("!! Anyone won the game !!");
+            }
+        }
+
+
+
+        // Function GetPlayerName
+        void GetPlayerName() {
+            Console.WriteLine(); // blank space
+            // Getting name of player 1
+            Console.Write("What the name of ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("Player 1");
+            Console.ResetColor();
+            Console.WriteLine(" ?");
+            player1.playerName = Console.ReadLine().ToUpper();
+            Console.WriteLine(); // blank space
+
+            // Getting name of player 2
+            Console.Write("What the name of ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("Player 2");
+            Console.ResetColor();
+            Console.WriteLine(" ?");
+            player2.playerName = Console.ReadLine().ToUpper();
+
+            // Hello message to both players
+            Console.WriteLine(); // blank space
+            Console.Write("Hello ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(player1.playerName);
+            Console.ResetColor();
+            Console.Write(" and ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(player2.playerName);
+            Console.ResetColor();
+            Console.Write(" welcome to the game ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Roll or Die");
+            Console.ResetColor();
+            Console.WriteLine(" !!");
+            Console.WriteLine(); // blank space
+        }
+
+        // Function that will ask if the player wants to play
+        internal void UserWantsToPlay() {
+            Console.Write("Do you want to play? write: ");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write("<YES>");
+            Console.ResetColor();
+            Console.Write(" or ");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("<NO>");
+            Console.ResetColor();
+            userStringWantsToPlay = Console.ReadLine().ToUpper();
+
+            if (userStringWantsToPlay == "YES" || userStringWantsToPlay == "Y") {
+                Console.WriteLine("You answared: YES");
+            }
+            else if (userStringWantsToPlay == "NO" || userStringWantsToPlay == "N") {
+                Console.WriteLine("You answared: NO");
+                Sorry();
+            }
+            while (userStringWantsToPlay != "YES" && userStringWantsToPlay != "Y" && userStringWantsToPlay != "NO" && userStringWantsToPlay != "N") {
+                Console.WriteLine(); // blank space
+                Console.WriteLine("--> !! Wrong input !! <--");
+                Console.WriteLine(); // blank space
+                UserWantsToPlay();
+            }
+        }
+
+        // Function UserUnderstoodRules check if the player understood the rules to preceed to play
+        internal void UserUnderstoodRules() {
+            Console.WriteLine(); // blank space
+            Console.Write("Did you understood the rules? (if yes press ");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write("<ENTER>");
+            Console.ResetColor();
+            Console.WriteLine(" to continue)");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+        }
+
+        // Function Intro
+        public void Intro() {
+            // Display the intro message
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("|Coded by -->Pedro Melo<-- on 10/03/2024|");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine
+                (" _   _      _ _ \r\n" +
+                "| | | | ___| | | ___  \r\n" +
+                "| |_| |/ _ \\ | |/ _ \\ \r\n" +
+                "|  _  |  __/ | | (_) |\r\n" +
+                "|_| |_|\\___|_|_|\\___/ ");
+            Console.ResetColor();
+            Console.WriteLine(); // blank space
+        }
+
+        // Function Description
+        public void Description() {
+            Console.WriteLine(); // blank space
+            Console.WriteLine("Okay! Now just read the rules of the game:");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("|----------------------------------------------------------------------------------------|");
+            Console.WriteLine("| In this game is you against or opponent and the game in based in four rounds.          |");
+            Console.WriteLine("| Every round the player 1 will pick one of the the dices and the same for the opponent. |");
+            Console.WriteLine("| You will pick one dice of this in here:                                                |");
+            Console.WriteLine("| -> 6 sides / -> 8 sides / -> 12 sides / -> 20 sides                                    |");
+            Console.WriteLine("| After you roll the dice we will calculate who won the round.                           |");
+            Console.WriteLine("| In the end the winner will be who has more round wins.                                 |");
+            Console.WriteLine("|----------------------------------------------------------------------------------------|");
+            Console.ResetColor();
+        }
+
+        // Function Sorry
+        public void Sorry() {
+            Console.WriteLine(); // blank space
+            Console.WriteLine("-- I understand that you don't want to play!! --");
+            End();
+            Environment.Exit(0);
+        }
+
+        // Function End
+        public void End() {
+            // Display the goodbye message
+            Console.WriteLine(); // blank space
+            Console.WriteLine("Goodbye!");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("|Coded by -->Pedro Melo<-- on10/03/2024|");
+            Console.ResetColor();
+        }
+
+    }
+}
---
+++ GD12_1133_A1_PedroMelo/GD12_1133_A1_PedroMelo/Scripts/Player.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GD12_1133_A1_PedroMelo.Scripts {
+    public class Player {
+        public int playerScore = 0;
+        public int playerTotalScore = 0;
+        public string playerName = "";
+        public string playerStringPickDice = "";
+
+        // Creating Dictionary of playerDicesAvailables to check what dices are availables
+        public Dictionary<string, bool> dicesAvailables = new Dictionary<string, bool>() {
+            { "isAvailableD6", true },
+            { "isAvailableD8", true },
+            { "isAvailableD12", true },
+            { "isAvailableD20", true }
+
+        };
+
+        // Creating the Instances
+        DiceRoller diceD6 = new DiceRoller(); // D6 instance
+        DiceRoller diceD8 = new DiceRoller(); // D8 instance
+        DiceRoller diceD12 = new DiceRoller(); // D12 instance
+        DiceRoller diceD20 = new DiceRoller(); // D20 instance
+
+        public void PlayerRollDice() {
+            diceD6.numberOfSides = 6;
+            diceD8.numberOfSides = 8;
+            diceD12.numberOfSides = 12;
+            diceD20.numberOfSides = 20;
+
+            if (playerStringPickDice == "6" && dicesAvailables["isAvailableD6"] == true) {
+                Console.WriteLine(); // blank space
+                diceD6.RollScore(ref playerScore);
+                dicesAvailables["isAvailableD6"] = false;
+            }
+            else if (playerStringPickDice == "8" && dicesAvailables["isAvailableD8"] == true) {
+                Console.WriteLine(); // blank space
+                diceD8.RollScore(ref playerScore);
+                dicesAvailables["isAvailableD8"] = false;
+            }
+            else if (playerStringPickDice == "12" && dicesAvailables["isAvailableD12"] == true) {
+                Console.WriteLine(); // blank space
+                diceD12.RollScore(ref playerScore);
+                playerTotalScore++;
+                dicesAvailables["isAvailableD12"] = false;
+            }
+            else if (playerStringPickDice == "20" && dicesAvailables["isAvailableD20"] == true) {
+                Console.WriteLine(); // blank space
+                diceD20.RollScore(ref playerScore);
+                dicesAvailables["isAvailableD20"] = false;
+            }
+            else {
+                Console.WriteLine(); // blank space
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write(playerName);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(" you need to select one of this dices: (6, 8, 12, 20)");
+                Console.WriteLine("And you can't select the same dice more than one time");
+                Console.ResetColor();
+                Console.WriteLine(); // blank space
+                // Call function PlayerPickDice
+                PlayerPickDice();
+            }
+        }
+
+        // Function PlayerPickDice get the input of which dice player will roll
+        public void PlayerPickDice() {
+            playerStringPickDice = "";
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(playerName);
+            Console.ResetColor();
+            Console.WriteLine(" turn:");
+            Console.WriteLine("Dices availables are 'true'");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine
+                ("D6: " + dicesAvailables["isAvailableD6"]
+                + "| D8: " + dicesAvailables["isAvailableD8"]
+                + "| D12: " + dicesAvailables["isAvailableD12"]
+                + "| D20: " + dicesAvailables["isAvailableD20"]);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(playerName);
+            Console.ResetColor();
+            Console.WriteLine(" which dice do you want to roll ?");
+            Console.WriteLine("--> 6 | 8 | 12 | 20 <--");
+            playerStringPickDice = Console.ReadLine();
+            // Call function PlayerRollDice
+            PlayerRollDice();
+        }
+    }
+}
